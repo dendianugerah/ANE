037ab907168ceaa92f96cdcb1bfdd8a0203999aa
Dendi <dendiaa@students.amikom.ac.id>
2024-12-08 21:09:18 +0700

    chore: update @tanstack/react-query to version 5.62.3 and refactor imports in useProducts hook

--- hooks/useProducts.ts
+++ hooks/useProducts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import type { Product } from "@/types/product";
 
 const fetchProducts = async (): Promise<Product[]> => {
