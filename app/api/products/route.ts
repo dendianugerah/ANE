025ca7701b202ef450155003f4df64df8553e18d
21ffiff <radeafb@gmail.com>
2024-12-08 19:14:30 +0800

    update ganti prisma

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,12 +1,30 @@
-import prisma from "@/lib/prisma";
+//import prisma from "@/lib/prisma";
+import { supabase } from "@/lib/supabase";
 import { NextResponse } from "next/server";
 
+// API menggunakan prisma:
+// export async function GET() {
+//     try {
+//         const products = await prisma.products.findMany();
+//         return NextResponse.json(products);
+//     } catch (error: unknown) {
+//         const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+//         return NextResponse.json({ error: errorMessage }, { status: 500 });
+//     }
+// }
+
 export async function GET() {
-    try {
-        const products = await prisma.products.findMany();
-        return NextResponse.json(products);
-    } catch (error: unknown) {
-        const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
-        return NextResponse.json({ error: errorMessage }, { status: 500 });
+    try{
+        const {data, error} = await supabase
+        .from("Products")
+        .select("*");
+        
+        if (error) throw error;
+
+        return NextResponse.json(data);
+    }catch (error: unknown){
+        const errorMessage = error instanceof Error ? error.message: "An unknown Error occured";
+        return NextResponse.json({error: errorMessage}, {status:500});
     }
+
 }
