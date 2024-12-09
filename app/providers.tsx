1db07f8077d817a3e221f096b5dc7f7de0cac2e2
Dendi <dendiaa@students.amikom.ac.id>
2024-12-09 09:55:34 +0700

    refactor: extract QueryClient initialization to a separate module and simplify Providers component

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,17 +1,12 @@
 "use client";
 
-import { useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "@/lib/queryClient";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient()
-  );
-
   return (
     <QueryClientProvider client={queryClient}>
       {children}
     </QueryClientProvider>
   );
-} 
+}
